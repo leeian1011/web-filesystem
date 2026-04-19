b0977f7fd4b72bf66f2db89e11c1da66b63c982d
Lee Ian <leeian1011@gmail.com>
2026-04-19 23:31:59 +0800

    feat: introduce index view and fs dir

--- app/controllers/filesystem_controller.rb
+++ app/controllers/filesystem_controller.rb
@@ -1,5 +1,13 @@
 class FilesystemController < ApplicationController
-  ROOT = Rails.root
+  ROOT = Rails.root.join("fs").to_s()
   def index
+    @files = Dir.children(ROOT).map do |name|
+      file = File.join(ROOT, name)
+      {
+        name: name,
+        dir?: File.directory?(file),
+        size: File.size(file)
+      }
+    end
   end
 end
