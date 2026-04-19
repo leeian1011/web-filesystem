6b1536c4aeaf8e96adb6b76f007981d627a49cd4
Lee Ian <leeian1011@gmail.com>
2026-04-19 22:42:30 +0800

    feat: bootstrap Rails

--- lib/main.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'server'
-
-if __FILE__ == $0
-  Server.new().serve()
-end
-
--- lib/parser.rb
+++
@@ -1,8 +0,0 @@
-class Parser
-  def self.parse(method)
-    # TODO: add regex to filter only for HTTP requests.
-    http_method = method[0...method.byteindex(' ')]
-    path = method[(http_method.length + 1)...method.byteindex(' ', http_method.length + 1)]
-    return { method: http_method, path: path }
-  end
-end
